03d9687907730c0f57243eb7ddd8c7bb3b5dd0a6
vishalverma96 <soni.vishu96@gmail.com>
2021-07-12 19:27:18 +0530

    project

---
+++ src/app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
+const app = express();
+const port = process.env.PORT || 8000; 
+
+//public static path
+const static_path = path.join(__dirname, "../public");
+const template_path = path.join(__dirname, "../templates/views");
+const partials_path = path.join(__dirname, "../templates/partials");
+
+app.set('view engine', 'hbs');
+app.set('views', template_path);
+hbs.registerPartials(partials_path);
+
+app.use(express.static(static_path));
+
+//home routing
+app.get("/",(req, res)=>{
+    res.render('index')
+})
+
+//about routng
+app.get("/about",(req, res)=>{
+    res.render('about')
+})
+
+//weather routing
+app.get("/weather",(req, res)=>{
+    res.render('weather')
+})
+
+//error routing
+app.get("*",(req, res)=>{
+    res.render('404error',{
+        errorMsg: 'Oopps Page not found'
+    })
+})
+
+
+app.listen(port, () => {
+    console.log('listening the port at 8000')
+})
